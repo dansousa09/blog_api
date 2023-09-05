1d24bbc1725c9f027b2a5a01c8fcbdbb306b3432
Danilove <danilo.elias@livsaude.com.br>
2023-09-05 15:05:50 -0700

    feat: create a helper handler for request in Create Post route

---
+++ handler/request.go
@@ -0,0 +1,33 @@
+package handler
+
+import "fmt"
+
+func errFieldIsRequired(name, typ string) error {
+	return fmt.Errorf("the %s field (%s) is required", name, typ)
+}
+
+type CreatePostRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func (r *CreatePostRequest) Validate() error {
+	if r.Title == "" && r.Content == "" && r.Author == "" {
+		return fmt.Errorf("the request body is unformatted or is empty")
+	}
+
+	if r.Title == "" {
+		return errFieldIsRequired("title", "string")
+	}
+
+	if r.Content == "" {
+		return errFieldIsRequired("content", "string")
+	}
+
+	if r.Author == "" {
+		return errFieldIsRequired("author", "string")
+	}
+
+	return nil
+}
