19b1695fcf692956764342255ca317270bb569c0
Danilove <danilo.elias@livsaude.com.br>
2023-09-05 15:12:45 -0700

    feat: create a helper handler for Success Responses

--- handler/response.go
+++ handler/response.go
@@ -1,10 +1,22 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func sendErrorResponse(ctx *gin.Context, statusCode int, message string) {
 	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(statusCode, gin.H{
 		"error": message,
+	})
+}
+
+func sendSuccessResponse(ctx *gin.Context, operation string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(200, gin.H{
+		"message": fmt.Sprintf("operation %s was successfully completed", operation),
+		"data":    data,
 	})
 }
