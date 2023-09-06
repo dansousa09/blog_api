589c8dda2e8c2f7e761925f26e1cec9809318739
Danilove <dani_love@bitbucket.org>
2023-09-05 22:58:31 -0300

    feat: create handler for Delete Post

--- handler/delete-post.go
+++ handler/delete-post.go
@@ -1,9 +1,41 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/dansousa09/blog_api/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	errFieldIsRequiredMsg = "field is required"
+	errPostNotFoundMsg    = "post not found"
+	errDeletePostErrorMsg = "error on delete post"
+)
 
 func DeletePostHandler(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "DeletePostHandler",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		err := errFieldIsRequired("id", "query-param")
+		logger.Errorf("error on get post: %s", err)
+		sendErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	post := schemas.Post{}
+
+	if err := db.First(&post, id).Error; err != nil {
+		logger.Errorf("error on get post: %s", err)
+		sendErrorResponse(ctx, http.StatusNotFound, errPostNotFoundMsg)
+		return
+	}
+
+	if err := db.Delete(&post).Error; err != nil {
+		logger.Errorf("%s: %s", errDeletePostErrorMsg, err)
+		sendErrorResponse(ctx, http.StatusInternalServerError, errDeletePostErrorMsg)
+		return
+	}
+
+	sendSuccessResponse(ctx, "delete post", post)
 }
